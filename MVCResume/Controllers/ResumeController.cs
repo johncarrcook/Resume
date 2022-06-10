f9660da1a2e71ff5b69cf1e2dc71a03690850627
John Cook <johncarrcook@gmail.com>
2022-06-10 18:20:07 -0400

    Created Data Models
    Refined Data models for EF

--- MVCResume/Controllers/ResumeController.cs
+++ MVCResume/Controllers/ResumeController.cs
@@ -6,7 +6,11 @@ namespace MVCResume.Controllers
     {
         public IActionResult Index()
         {
-            
+            ViewBag.ResumeSummary = "Motivated problem solver with a proven track record of integrity and dependency. " +
+                "Driven to learn new skills as evidenced by completing a certificate program in computer programming " +
+                "while working full time over the past year. Strong communication skills and experience working with " +
+                "customers and colleagues. Equally as effective at working in a team as working in self-directed settings " +
+                "to create high quality, innovative work. ";
             return View();
         }
     }
--- MVCResume/Models/Resume.cs
+++ MVCResume/Models/Resume.cs
@@ -3,9 +3,11 @@
     public class Resume
     {
         public int Id { get; set; }
-        public string? Duration { get; set; }
+        public string? StartDate { get; set; }
+        public string? EndDate { get; set; }
         public string? Title { get; set; }
         public string? InstName { get; set; }
+        public string? Location { get; set; }
         public List<Experience>? Experiences { get; set; } = new List<Experience>();
     }
 }
