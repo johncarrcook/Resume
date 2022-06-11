dc6048305113b814270f70361fae343576d4d78a
John Cook <johncarrcook@gmail.com>
2022-06-10 21:16:56 -0400

    Added EntityFramework

---
+++ MVCResume/Data/MVCResumeContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCResume.Models;
+
+namespace MVCResume.Data
+{
+    public class MVCResumeContext : DbContext
+    {
+        public MVCResumeContext (DbContextOptions<MVCResumeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCResume.Models.Resume>? Resume { get; set; }
+        public DbSet<MVCResume.Models.Experience>? Experience { get; set; }
+    }
+}
